0327df79a1fa1fe865990272e79127a1bb006b98
Nabilouz <nabiloufarhanos@protonmail.com>
2022-08-15 15:57:26 +0200

    FirstCommit

---
+++ CalendarConsole/Program.cs
@@ -0,0 +1,12 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Ical.Net;
+
+Console.WriteLine("Hello, World!");
+
+public class MyClass
+{
+    Calendar testcal = new Calendar();
+    string test = "";
+    
+
+}
