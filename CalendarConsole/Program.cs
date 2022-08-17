2103a52c9eea165e9518c233d5251cc1852e9dc7
Nabilouz <nabiloufarhanos@protonmail.com>
2022-08-17 15:54:01 +0200

    Building events objects to include in calendar

--- CalendarConsole/Program.cs
+++ CalendarConsole/Program.cs
@@ -1,13 +1,30 @@
 ﻿// See https://aka.ms/new-console-template for more information
 using Ical.Net;
+using System.Net.Http;
 
-Console.WriteLine("Hello, World!");
+//Console.WriteLine("Hello, World!");
 
-public class MyClass
+
+const string UriServer = "http://localhost:5232/test/d2c0d129-3f0c-dc0d-8ef9-13c95f0db47b/";
+HttpClient client = new HttpClient();
+Calendar testcal = new Calendar();
+var response = client.GetStringAsync(UriServer).Result;
+
+var calCollection = Calendar.Load(response);
+
+EventModel eventM = new();
+
+eventM.Id = calCollection.Events[0].Uid;
+eventM.Summary = calCollection.Events[0].Summary;
+eventM.StartDate = calCollection.Events[0].DtStart.Value;
+eventM.EndDate = calCollection.Events[0].DtEnd.Value;
+eventM.Sequence = calCollection.Events[0].Sequence;
+
+public class EventModel 
 {
-    Calendar testcal = new Calendar();
-    string test = "";
-
-    string testMe = "TestMe";    
-
+        public string Id { get; set; }
+        public string Summary { get; set; }
+        public DateTime StartDate { get; set; }
+        public DateTime EndDate { get; set; }
+        public int Sequence { get; set; }
 }
