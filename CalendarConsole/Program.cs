04c438f955ae362010061955f7ae4a18b8734ace
Nabilouz <nabiloufarhanos@protonmail.com>
2022-08-21 10:42:49 +0200

    Prepping calendar objects

--- CalendarConsole/Program.cs
+++ CalendarConsole/Program.cs
@@ -1,5 +1,7 @@
 ﻿// See https://aka.ms/new-console-template for more information
 using Ical.Net;
+using Ical.Net.CalendarComponents;
+using Ical.Net.DataTypes;
 using System.Net.Http;
 
 //Console.WriteLine("Hello, World!");
@@ -20,10 +22,27 @@ eventM.StartDate = calCollection.Events[0].DtStart.Value;
 eventM.EndDate = calCollection.Events[0].DtEnd.Value;
 eventM.Sequence = calCollection.Events[0].Sequence;
 
+System.Console.WriteLine(eventM.ToString());
+
+//EventModel eventTest = new();
+CalendarEvent eventTest = new();
+
+//eventTest.Id = Guid.NewGuid().ToString();
+eventTest.Summary = "testEvent";
+eventTest.DtStart = new CalDateTime(DateTime.UtcNow);
+eventTest.DtEnd = new CalDateTime(DateTime.UtcNow.AddHours(1));
+eventTest.Sequence = 2;
+
+calCollection.Events.Add(eventTest);
+
+//Add post request
+
+
+
 public class EventModel 
 {
-        public string Id { get; set; }
-        public string Summary { get; set; }
+        public string? Id { get; set; }
+        public string? Summary { get; set; }
         public DateTime StartDate { get; set; }
         public DateTime EndDate { get; set; }
         public int Sequence { get; set; }
